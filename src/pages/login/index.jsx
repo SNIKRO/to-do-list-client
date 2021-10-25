a0063fdaa0008086b78f85683bcdefb0fb083cf7
SNIKR <snikr@bk.ru>
2021-10-25 23:55:23 +0600

    fix links

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -11,19 +11,22 @@ import {
   Typography,
   Snackbar,
   Alert,
+  TextField,
 } from '@mui/material';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import CloseIcon from '@mui/icons-material/Close';
 import { useState } from 'react';
 import { logIn } from '../../api/user';
-import { REGISTRATION } from '../../routing/routes';
+import { MAIN, REGISTRATION } from '../../routing/routes';
+import { Link, useHistory } from 'react-router-dom';
 
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [authError, setAuthError] = useState('');
+  const history = useHistory();
 
   function handleShowPasswordClick() {
     setShowPassword(!showPassword);
@@ -48,6 +51,7 @@ export default function Login() {
   async function handleFormSubmit() {
     try {
       await logIn(email, password);
+      history.push(MAIN);
     } catch (error) {
       setAuthError(error.message);
     }
@@ -60,18 +64,17 @@ export default function Login() {
           <Typography variant="h4" gutterBottom>
             Log In Form
           </Typography>
-          <FormControl fullWidth>
-            <InputLabel htmlFor="email">Email address</InputLabel>
-            <Input
-              id="email"
-              type="email"
-              aria-describedby="my-helper-text"
-              value={email}
-              onChange={handleEmailChange}
-              placeholder="input your email"
-            />
-          </FormControl>
-          <FormControl fullWidth>
+          <TextField
+            fullWidth
+            type="email"
+            value={email}
+            label="Email"
+            variant="standard"
+            onChange={handleEmailChange}
+            placeholder="input your email"
+          />
+
+          <FormControl fullWidth variant="standard">
             <InputLabel htmlFor="password">Password</InputLabel>
             <Input
               id="password"
@@ -92,8 +95,8 @@ export default function Login() {
           <Button variant="contained" onClick={handleFormSubmit}>
             Log In
           </Button>
-          <Button variant="outlined" href={REGISTRATION}>
-            REGISTRATION
+          <Button variant="outlined">
+            <Link to={REGISTRATION}>REGISTRATION</Link>
           </Button>
           <Snackbar open={!!authError}>
             <Alert
--- src/pages/registration/index.jsx
+++ src/pages/registration/index.jsx
@@ -10,12 +10,13 @@ import {
   Button,
   Typography,
   TextField,
+  FormHelperText,
 } from '@mui/material';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import { useState } from 'react';
 import { registration } from '../../api/user';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { LOG_IN } from '../../routing/routes';
 import validator from 'validator';
 
@@ -116,8 +117,8 @@ export default function RegistrationForm() {
             placeholder="input your email"
           />
 
-          <FormControl fullWidth error={!!passwordValidation}>
-            <InputLabel htmlFor="password">{!!passwordValidation ? passwordValidation : 'Password'}</InputLabel>
+          <FormControl fullWidth error={!!passwordValidation} variant="standard">
+            <InputLabel htmlFor="password">Password</InputLabel>
             <Input
               id="password"
               onChange={handlePasswordChange}
@@ -131,14 +132,14 @@ export default function RegistrationForm() {
                   </IconButton>
                 </InputAdornment>
               }
-              label="Password"
             />
+            {passwordValidation ? <FormHelperText>{passwordValidation}</FormHelperText> : null}
           </FormControl>
           <Button variant="contained" onClick={handleFormSubmit}>
             Registration
           </Button>
-          <Button variant="outlined" href={LOG_IN}>
-            Log In
+          <Button variant="outlined">
+            <Link to={LOG_IN}>Log In</Link>
           </Button>
         </Stack>
       </Box>
